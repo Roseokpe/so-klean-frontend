00e9e4b45420c5883cf19cf8f52839a59649ad4d
“businesskaniki” <businesskanikii@gmail.com>
2023-01-10 11:25:24 +0300

    adding a form to add cleaners

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="homecont">
+    <div className="text">
+      <h4>BEST CLEANERS</h4>
+      <p>please select a cleaner for hire</p>
+    </div>
+  </div>
+);
+
+export default Home;
