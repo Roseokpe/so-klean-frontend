b32b540bf1b013206abf7f5d853aaef1e36748c9
roseokpe <roseokpe@gmail.com>
2023-01-11 16:54:39 -0800

    make changes

--- src/components/cleanerDetails.jsx
+++ src/components/cleanerDetails.jsx
@@ -10,21 +10,17 @@ function CleanerDetails() {
           </div>
           <div className="cleaner-description">
             <div className="descr-top">
-              <h4>Christiana Jose</h4>
-              <p>In publishing and graphic design, Lorem ipsum is a placeholder<br/>
-                text commonly used to demonstrate the visual form of<br/>
-                a document or a typeface without relying on meaningful content.<br/>
-                Lorem ipsum may be used as a placeholder before final<br/>
-              </p>
+              <h4>cleaner name</h4>
+              <p>Sample Rich</p>
             </div>
             <div className="descr-btm">
               <p>
-                Price :
+                <h4>charges</h4>
               </p>
-              <p>
+              <p className="Date">
                 Date
               </p>
-              <p>
+              <p className="Location">
                 Lagos
               </p>
             </div>
