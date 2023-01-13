3fcc2fa9e30700083123dcd07a6083621535ba0b
“businesskaniki” <businesskanikii@gmail.com>
2023-01-13 14:04:05 +0300

    corrected the inputs and the requested changes

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,12 +5,12 @@ function Signup() {
   return (
     <div>
       <form>
-        <h3>Login Here</h3>
-        <label htmlFor="username">Username</label>
+        <h3>signup here</h3>
+        <label htmlFor="email">Username</label>
         <input type="text" placeholder="Email or Phone" id="username" />
         <label htmlFor="password">Password</label>
         <input type="password" placeholder="Password" id="password" />
-        <button type="button">Log In</button>
+        <button type="button">sign up</button>
       </form>
     </div>
 
